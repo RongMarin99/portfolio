ced0fc8e573c6a31b8512778e86ba87b201f4572
rongmarin <mrlucky5868@gmail.com>
2023-03-01 21:57:37 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og-type', property: 'og:type', content: 'website' },
+          { 
+            hid: 'og-image', property: 'og:image',
+            content: require('../assets/image/logo.jpg')
+          },
+          { hid: 'og-url', property: 'og:url', content: 'https://effortless-froyo-c29932.netlify.app/' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
