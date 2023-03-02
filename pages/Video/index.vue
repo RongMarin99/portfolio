c4b2157b19587a063a2212014af650ca4c0b7091
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-02 15:51:09 +0700

    update

--- pages/Video/index.vue
+++ pages/Video/index.vue
@@ -1,7 +1,7 @@
 <template>
     <b-container class="main-slide" >
         <b-row>
-            <b-col xl="3" lg="3" md="4" sm="4" cols="12" v-for="item in videos" :key="item" >
+            <b-col xl="3" lg="3" md="4" sm="6" cols="12" v-for="item in videos" :key="item" >
                 <iframe width="100%" style="height:15rem" :src="item['url']" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                   <p :class="$colorMode.value=='dark'?'text-light-mode' : 'text-light-mode'" class="pt-2">
                    {{ item['title'] }}
