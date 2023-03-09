e612c1f873fed741dd11ba00e847306dea466688
rongmarin <mrlucky5868@gmail.com>
2023-03-10 00:14:27 +0700

    update share

--- pages/Detail/index.vue
+++ pages/Detail/index.vue
@@ -4,7 +4,7 @@
             data-href="https://etec-center1.netlify.app/Detail?id=22&type=1" 
             data-layout="button_count">
         </div>
-        <a href="https://www.facebook.com/sharer.php?u=link">Facebook</a>
+        <a href="https://www.facebook.com/sharer.php?u=https://etec-center1.netlify.app/Detail?id=7&type=2&image=6405f6dcea9651.30450625.png">Facebook</a>
         <b-row>
             <b-col cols="12" xl="8" lg="12" md="12" xs="12" class="mt-3">
                <h3>{{ getNameByLocalByLang(detail['title']) }}​</h3>
