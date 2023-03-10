541fae81d8c4e481ef18c04c377419999a7c699e
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-10 15:05:16 +0700

    update

--- pages/Detail/index.vue
+++ pages/Detail/index.vue
@@ -7,7 +7,7 @@
         <a href="https://www.facebook.com/sharer.php?u=https://etec-center1.netlify.app/Detail?id=7&type=2&image=6405f6dcea9651.30450625.png">Facebook</a>
         <b-row>
             <b-col cols="12" xl="8" lg="12" md="12" xs="12" class="mt-3">
-               <h3>{{ getNameByLocalByLang(detail['title']) }}​</h3>
+               <h3>{{ getNameByLocalByLang(meta.title) }}​</h3>
                <b-row>
                  <b-col cols="12" xl="6" lg="6" md="6" xs="6" class="mt-3">
                     <span>
