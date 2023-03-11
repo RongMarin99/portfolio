6e7681db4e3da6df16ffab2fdc2c15e7e2feb27c
rongmarin <mrlucky5868@gmail.com>
2023-03-11 23:10:10 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   mode: "universal", 
   target: "static",
-  ssr: false,
+  ssr: true,
   generate: {
     fallback: true
   },
