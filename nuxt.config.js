1bffb1d8c9f8f60719cda831a372ca0ced470031
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-15 11:18:47 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,19 +151,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    analyze: true,
-    // Add exception
-    transpile: ["vee-validate/dist/rules"],
-    html: {
-      minify: {
-        collapseWhitespace: true, // as @dario30186 mentioned
-        removeComments: true, // 👈 add this line
-      },
-    },
-    postcss: {
-      plugins: {
-      },
-    },
+    extend (config) {
+      config.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      })
+    }
   },
   loading: {
     color: '#FD7237',
