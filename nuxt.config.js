94f669bb7ece55c52af2b948eb3a081e2cdb568c
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-15 13:12:45 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,8 +151,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend(config) {
-      config.resolve.alias['node-fetch-native'] = require.resolve('node-fetch')
+    extend (config) {
+      config.module.rules.push({
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto"
+      })
     }
   },
   loading: {
