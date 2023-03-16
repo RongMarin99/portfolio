4f55794c8d4e2239f9f98414536ebed21096aacc
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-16 15:27:01 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -145,6 +145,7 @@ export default {
     background: 'white'
   },
   env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+    WEB_URL: process.env.WEB_URL
   }
 }
