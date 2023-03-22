d72f1eff5a4dbb75c495692a461e815373d36692
pheaktraZ <119937466+Pheaktra7@users.noreply.github.com>
2023-03-22 20:02:45 +0700

    update

--- pages/Detail/_id/_type.vue
+++ pages/Detail/_id/_type.vue
@@ -80,6 +80,10 @@ export default{
             hid: 'og-image', property: 'og:image',
             content: this.api_key+"/image_etec/"+this.meta.image
           },
+          { 
+            hid: 'og-url', property: 'og:url', 
+            content: this.web_url+"/Detail/"+this.$route.params.id+"/"+this.$route.params.type
+          },
           { property: "og:image:width", content: "740" },
           { property: "og:image:height", content: "300" },
         ]
