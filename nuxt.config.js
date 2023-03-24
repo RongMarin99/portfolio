63a6de496441ed52d2d17fbff0cb64f93fc9730f
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-03-24 17:18:25 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,11 +113,7 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: 'https://etec-api.loveounnas.xyz/api/',
     proxy: true,
-    credentials: false
    // baseURL: 'http://localhost:8000/api/',
-  },
-  proxy: {
-    changeOrigin: true,
   },
   manifest: {
     name: 'Etec Center',
