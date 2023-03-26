e6b58c121120cf216558df99358662c860aa8dde
rongmarin <mrlucky5868@gmail.com>
2023-03-26 14:59:19 +0700

    üpdate google search

--- layouts/default.vue
+++ layouts/default.vue
@@ -154,9 +154,34 @@ export default{
   //     ]
   //   }
   // },
-  head () {
-    return this.$nuxtI18nHead()
-  },
+    head () {
+      return {
+        title: "KroIT | Etec Center",
+        htmlAttrs: {
+          lang: this.$i18n.locale
+        },
+        meta: [
+          {
+            hid: "og:title",
+            property: "og:title",
+            content: "KroIT | ETEC I.T Training Center",
+          },
+          { 
+            hid: 'og:image', property: 'og:image', content: require('../assets/image/logo.jpg')
+          },
+          {
+            hid: "og:description",
+            name: 'og:description',
+            property: "og:description",
+            content: "ETEC I.T Training Center is a School located in Phnom Penh, KH. The business is listed under  school category.It has received 1 reviews with an average rating of 3 stars.",
+          },
+          { 
+            hid: 'og-url', property: 'og:url', 
+            content: this.web_url
+          },
+        ]
+      }
+    },
     data(){
         return {
           api_key: process.env.BASE_URL,
