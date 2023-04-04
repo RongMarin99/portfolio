7be2ac40a4dd206db30a1d7948bfabe52deaaec4
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-04-04 10:08:51 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -145,9 +145,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://api.kroit.xyz/api/',
+   // baseURL: 'https://api.kroit.xyz/api/',
     proxy: true,
    // baseURL: 'http://localhost:8000/api/',
+  },
+  proxy: {
+    target: `${process.env.BASE_URL}/api/`,
   },
   manifest: {
     name: 'Etec Center',
