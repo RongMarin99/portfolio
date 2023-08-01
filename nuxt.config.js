45f6360322d6671cb8885b8257a758f99372b452
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-08-01 17:08:58 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/google-analytics'
+    '@nuxtjs/google-analytics',
+    'nuxt-compress'
   ],
   googleAnalytics: {
     id: '359938223'
