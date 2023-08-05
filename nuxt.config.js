3ee9634ea3ee5e31a50da524e8ca927fce3035b7
rongmarin <mrlucky5868@gmail.com>
2023-08-05 13:47:18 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -167,9 +167,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend(config) {
-      config.resolve.alias['node-fetch-native'] = require.resolve('node-fetch')
-    }
+    // extend(config) {
+    //   config.resolve.alias['node-fetch-native'] = require.resolve('node-fetch')
+    // }
   },
   loading: {
     color: '#6f34fe',
